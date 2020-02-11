87b16cbd45aad9094508194cfdfd2fb9b1012057
unknown <a.yanovska@student.csn.khai.edu>
2020-02-11 13:26:15 +0200

    my 3 commit

--- sp_lab0/sp_lab1/Source.cpp
+++ sp_lab0/sp_lab1/Source.cpp
@@ -8,29 +8,16 @@ int main(int argc, char* argv[])
 {
 	cout << "     data type      " << "byte" << "      " << "    min value ... max value  " << endl 
 
-		<< "char               =  " << sizeof(char) << "            " << (CHAR_MIN) << " ... " << fixed << setprecision(2)
-		<< (CHAR_MAX) << endl
-		<< "short              =  " << sizeof(short int) << "            " << (SHRT_MIN) << " ... " << fixed << setprecision(2)
-		 << (SHRT_MAX) << endl
-		<< "int                =  " << sizeof(int) << "            " << (INT_MIN) << " ... " << fixed << setprecision(2)
-		 << (INT_MAX) << endl
-		<< "long               =  " << sizeof(long int) << "            " << (LONG_MIN) << " ... " << fixed << setprecision(2)
-		 << (LONG_MAX) << endl
-		<< "long long          =  " << sizeof(long long) << "            " << (LLONG_MIN) << " ... " << fixed << setprecision(2)
-		 << (LLONG_MAX) << endl
-		
-		<< "unsigned char      =  " << sizeof(unsigned char) << "         " <<  "   0"  << " ... " << fixed << setprecision(2)
-		 << (UCHAR_MAX) << endl
-		<< "unsigned short     =  " << sizeof(unsigned short int) << "         " << "   0" << " ... " << fixed << setprecision(2)
-		 << (USHRT_MAX)<< endl
-
-		<< "unsigned int       =  " << sizeof(unsigned int) << "         " << "   0" << " ... " << fixed << setprecision(2)
-		 << (UINT_MAX) << endl
-
-		<< "unsigned long      =  " << sizeof(unsigned long int) << "         " << "   0" << " ... " << fixed << setprecision(2)
-		 << (ULONG_MAX) << endl
-		<< "unsigned long long =  " << sizeof(unsigned long long) << "         " <<"   0" << " ... " << fixed << setprecision(2)
-		 << (ULLONG_MAX) << endl;
+		<< "char               =  " << sizeof(char) << "            " << (CHAR_MIN) << " ... " << (CHAR_MAX) << endl
+		<< "short              =  " << sizeof(short int) << "            " << (SHRT_MIN) << " ... " << (SHRT_MAX) << endl
+		<< "int                =  " << sizeof(int) << "            " << (INT_MIN) << " ... "  << (INT_MAX) << endl
+		<< "long               =  " << sizeof(long int) << "            " << (LONG_MIN) << " ... " << (LONG_MAX) << endl
+		<< "long long          =  " << sizeof(long long) << "            " << (LLONG_MIN) << " ... " << (LLONG_MAX) << endl
+		<< "unsigned char      =  " << sizeof(unsigned char) << "         " <<  "   0"  << " ... " << (UCHAR_MAX) << endl
+		<< "unsigned short     =  " << sizeof(unsigned short int) << "         " << "   0" << " ... " << (USHRT_MAX)<< endl
+		<< "unsigned int       =  " << sizeof(unsigned int) << "         " << "   0" << " ... " << (UINT_MAX) << endl
+		<< "unsigned long      =  " << sizeof(unsigned long int) << "         " << "   0" << " ... " << (ULONG_MAX) << endl
+		<< "unsigned long long =  " << sizeof(unsigned long long) << "         " <<"   0" << " ... " << (ULLONG_MAX) << endl;
 		
 		
 	system("pause");
