4fe2d01a0068cdf18ce38f769ecd7e0f26a12c1d
unknown <a.yanovska@student.csn.khai.edu>
2020-05-20 13:01:18 +0300

    Lab 4

---
+++ sp_lab4/sp_lab4.1/sp_lab4.1/sp_lab4.1.cpp
@@ -0,0 +1,142 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <Windows.h>
+#include <locale.h>
+
+#define bSize 1024
+#define maxNumberOfFiles 10
+
+
+void incorrect_input() {
+	printf("\n***********You made a mistake************\n");
+	printf("Only 2 arguments available:\n");
+	printf("1. PACK\n");
+	printf("2. UNPACK\n");
+	exit(0);
+}
+
+void print_problems() {
+	setlocale(LC_ALL, "Russian");
+	printf("\n******Something went wrong*****\n");
+	LPVOID e_mess;
+	DWORD e_code = GetLastError();
+	FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM, NULL,
+		e_code, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPTSTR)&e_mess, 0, NULL);
+	char* err_mess = (char*)e_mess;
+	wprintf(L"ERROR was generated: failed with 0x%x code\n", e_code);
+	wprintf(L"ERROR formatted message: %s\n", err_mess);
+}
+
+bool pack_files()
+{
+	bool result = true;
+	LPSTR* inputFiles = new CHAR * [maxNumberOfFiles];
+	int inputFilesCount;
+	printf("Input number of files, which will archive: ");
+	scanf("%d", &inputFilesCount);
+	printf("Input full path to files, which will archive\n");
+	LPSTR fileName = new CHAR[MAX_PATH];
+	for (int i = 0; i < inputFilesCount; i++) {
+		printf("Input full path to file:");
+		scanf("%s", fileName);
+		inputFiles[i] = new CHAR[MAX_PATH];
+		strcpy(inputFiles[i], fileName);
+	}
+	LPSTR archiveName = new CHAR[MAX_PATH];
+	printf("Input full path to new archive: ");
+	scanf("%s", archiveName);
+	HANDLE hInReadPipe;
+	HANDLE hInWritePipe;
+	HANDLE hOutReadPipe;
+	HANDLE hOutWritePipe;
+	CreatePipe(&hInReadPipe, &hInWritePipe, NULL,0);	     
+	CreatePipe(&hOutReadPipe, &hOutWritePipe, NULL, 0);
+	if (hInReadPipe == INVALID_HANDLE_VALUE || hInWritePipe == INVALID_HANDLE_VALUE ||
+		hOutReadPipe == INVALID_HANDLE_VALUE || hOutWritePipe == INVALID_HANDLE_VALUE) {
+		print_problems();
+		result = false;
+	}
+	STARTUPINFOA startInfo;
+	ZeroMemory(&startInfo, sizeof(startInfo));
+	startInfo.cb = sizeof(startInfo);		
+	startInfo.hStdInput = hInReadPipe;		
+	startInfo.hStdOutput = hOutWritePipe;		
+	startInfo.hStdError = hOutWritePipe;		
+	startInfo.dwFlags |= STARTF_USESTDHANDLES;
+	PROCESS_INFORMATION procInfo;
+	ZeroMemory(&procInfo, sizeof(procInfo));
+	LPSTR cmdLine = new CHAR[MAX_PATH];
+	ZeroMemory(cmdLine, MAX_PATH);
+	strncpy(cmdLine, "7z.exe a ", MAX_PATH - strlen(cmdLine));
+	strncat(cmdLine, archiveName, MAX_PATH - strlen(cmdLine));
+	for (int i = 0; i < inputFilesCount; i++)
+	{
+		strncat(cmdLine, " ", MAX_PATH - strlen(cmdLine));
+		strncat(cmdLine, inputFiles[i], MAX_PATH - strlen(cmdLine));
+	}
+	strncat(cmdLine, " ", MAX_PATH - strlen(cmdLine));
+	if (!CreateProcessA(NULL, cmdLine, NULL, NULL, TRUE, NORMAL_PRIORITY_CLASS, NULL, NULL, &startInfo, &procInfo)) {
+		print_problems();
+		result = false;
+	}
+	if (result) {
+		WaitForSingleObject(procInfo.hProcess, INFINITE);
+		DWORD readBytes = 0;
+		LPSTR outBuffer = new CHAR[bSize];
+		ZeroMemory(outBuffer, bSize);
+		OVERLAPPED overlapped;
+		ReadFile(hOutReadPipe, outBuffer, bSize, &readBytes, &overlapped);
+		CloseHandle(procInfo.hProcess);
+		CloseHandle(procInfo.hThread);
+	}
+	CloseHandle(hOutReadPipe);
+	CloseHandle(hOutWritePipe);
+	CloseHandle(hInReadPipe);
+	CloseHandle(hInWritePipe);
+	return result;
+}
+
+bool unpack_files()
+{
+	bool result = true;
+	LPSTR inputFileName = new CHAR[MAX_PATH];
+	LPSTR outputFileName = new CHAR[MAX_PATH];
+	printf("Input full path to archive: ");
+	scanf("%s", inputFileName);
+	printf("Input full output path: ");
+	scanf("%s", outputFileName);
+	LPSTR cmdLine = new CHAR[MAX_PATH];
+	ZeroMemory(cmdLine, MAX_PATH);
+	// e (Extract) command
+	strncpy(cmdLine, "7z.exe e ", MAX_PATH - strlen(cmdLine));
+	strncat(cmdLine, inputFileName, MAX_PATH - strlen(cmdLine));
+	// -o (set Output directory) switch
+	strncat(cmdLine, " -o", MAX_PATH - strlen(cmdLine));
+	strncat(cmdLine, outputFileName, MAX_PATH - strlen(cmdLine));
+	// -y (assume Yes on all queries) switch
+	strncat(cmdLine, " -y", MAX_PATH - strlen(cmdLine));
+	STARTUPINFOA startInfo;
+	ZeroMemory(&startInfo, sizeof(startInfo));
+	startInfo.cb = sizeof(startInfo);
+	startInfo.dwFlags |= STARTF_USESTDHANDLES;
+	PROCESS_INFORMATION procInfo;
+	ZeroMemory(&procInfo, sizeof(procInfo));
+	if (!CreateProcessA(NULL, cmdLine, NULL, NULL, TRUE, NORMAL_PRIORITY_CLASS, NULL, NULL, &startInfo, &procInfo)) {
+		print_problems();
+		result = false;
+	}
+	return result;
+}
+int main(int argc, char* argv[])
+{
+	if (argc != 2 || ((strcmp(argv[1], "PACK") != 0) && (strcmp(argv[1], "UNPACK")) != 0)) {
+		incorrect_input();
+	}
+	if (strcmp(argv[1], "PACK") == 0) {
+		(pack_files()) ? printf("********The files were archived*************") : printf("************The files were not archived************8");
+	}
+	else {
+		(unpack_files()) ? printf("**************The files were unziped***********") : printf("***********The files were not unziped**************");
+	}
+	return 0;
+}
---
+++ sp_lab4/sp_lab4.2/sp_lab4.2/sp_lab4.2.cpp
@@ -0,0 +1,102 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <Windows.h>
+#include <time.h>
+
+#define amountOfNumbers 5
+
+CRITICAL_SECTION criticalSection;
+HANDLE* threadHandles;
+int dwTlsIndex;
+
+
+void incorrect_input() {
+	printf("\n**************You made a mistake*********\n");
+	printf("Only 1 arguments available:\n");
+	printf("Number of threads => more then 1\n");
+	exit(0);
+}
+
+int countLargestDivisor(int number)
+{
+	int n = number / 2;
+	for (int i = n; i >= 2; i--) {
+		if (number % i == 0)
+			return i;
+	}
+}
+
+void fillLargestDivisors(int* initArr, int* resArr)
+{
+	for (int i = 0; i < amountOfNumbers; i++) {
+		resArr[i] = countLargestDivisor(initArr[i]);
+	}
+}
+
+void printArr(int* arr, int size)
+{
+	for (int i = 0; i < size; i++) {
+		printf(" %d", arr[i]);
+	}
+}
+
+int countSum(int* arr, int size)
+{
+	int result = 0;
+	for (int i = 0; i < size; i++) {
+		result += arr[i];
+	}
+	return result;
+}
+
+DWORD WINAPI executedByThread(LPVOID arr)
+{
+	EnterCriticalSection(&criticalSection);
+	printf("Id\n %u\n", GetCurrentThreadId());
+	int* array = (int*)arr;
+	TlsSetValue(dwTlsIndex, (LPVOID)(new int[amountOfNumbers]));
+	int sum = 0;
+	printf("Generated array\n ");
+	printArr(array, amountOfNumbers);
+	fillLargestDivisors(array, (int*)TlsGetValue(dwTlsIndex));
+	printf("\nModyfied array\n ");
+	printArr((int*)TlsGetValue(dwTlsIndex), amountOfNumbers);
+	sum = countSum((int*)TlsGetValue(dwTlsIndex), amountOfNumbers);
+	printf("\nSum\n %d\n\n", sum);
+	LeaveCriticalSection(&criticalSection);
+	return 0;
+}
+
+void runThreads(int threadsCount)
+{
+	printf("Number of threads \n %d\n\n", threadsCount);
+	InitializeCriticalSection(&criticalSection);
+	threadHandles = new HANDLE[threadsCount];
+	DWORD* ids = new DWORD[threadsCount];
+	int** arrays = new int* [threadsCount];
+	time_t t;
+	srand((unsigned)time(&t));
+	dwTlsIndex = TlsAlloc();
+	for (int i = 0; i < threadsCount; i++)
+	{
+		arrays[i] = new int[amountOfNumbers];
+		for (int j = 0; j < amountOfNumbers; j++) {
+			arrays[i][j] = rand() % 90 + 10;
+		}
+		threadHandles[i] = CreateThread(NULL,0,executedByThread, arrays[i],	NULL,&ids[i]);		  
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	if (argc != 2) {
+		incorrect_input();
+	}
+	else {
+		int threadsCount = atoi(argv[1]);
+		runThreads(threadsCount);
+		WaitForMultipleObjects(threadsCount, threadHandles, TRUE, INFINITE);
+		DeleteCriticalSection(&criticalSection);
+	}
+	return 0;
+}
